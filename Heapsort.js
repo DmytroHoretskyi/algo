40ba534190ef1b19ecb3ddabdd855f01bcce893d
DmytroHoretskyi <serwserw16@gmail.com>
2021-11-04 16:37:49 +0200

    update algorithm

--- Heapsort.js
+++ Heapsort.js
@@ -1,60 +1,55 @@
-function heapifyMax(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
+function heapify(
+    arr,
+    length,
+    i,
+    counter = { comparisons: 0, swaps: 0, executionTime: 0 },
+    asc = true,
+) {
     let largest = i;
     let left = i * 2 + 1;
     let right = left + 1;
 
-
     counter.comparisons++;
+
     if (left < length) {
         counter.comparisons++; //for next 'if'
-        if(arr[left] > arr[largest]) {
-            largest = left;
+
+        if(asc) {
+            if(arr[left] > arr[largest]) {
+                largest = left;
+            }
+        }
+
+        if(!asc) {
+            if(arr[left] < arr[largest]) {
+                largest = left;
+            }
         }
     }
 
     counter.comparisons++;
+
     if (right < length) {
         counter.comparisons++; //for next 'if'
-        if(arr[right] > arr[largest]) {
 
-            largest = right;
+        if (asc) {
+            if (arr[right] > arr[largest]) {
+                largest = right;
+            }
+        }
+
+        if (!asc) {
+            if (arr[right] < arr[largest]) {
+                largest = right;
+            }
         }
     }
 
-    if(largest != i) {
+    if(largest !== i) {
         counter.swaps++;
         [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapifyMax(arr, length, largest, counter);
-    }
-}
 
-function heapifyMin(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
-    let largest = i;
-    let left = i * 2 + 1;
-    let right = left + 1;
-
-
-    counter.comparisons++;
-    if (left < length) {
-        counter.comparisons++; //for next 'if'
-        if(arr[left] < arr[largest]) {
-            largest = left;
-        }
-    }
-
-    counter.comparisons++;
-    if (right < length) {
-        counter.comparisons++; //for next 'if'
-        if(arr[right] < arr[largest]) {
-
-            largest = right;
-        }
-    }
-
-    if(largest != i) {
-        counter.swaps++;
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapifyMin(arr, length, largest, counter);
+        heapify(arr, length, largest, counter, asc);
     }
 }
 
@@ -71,32 +66,32 @@ function heapSort(arr, sortOrder = "asc") {
 
     if(sortOrder === "asc" ) {
         while (i >= 0) {
-            heapifyMax(arr, length, i, counter);
+            heapify(arr, length, i, counter, true);
             i--;
         }
 
         while (k >= 0) {
             counter.swaps++;
             [arr[0], arr[k]] = [arr[k], arr[0]];
-            heapifyMax(arr, k, 0, counter);
+            heapify(arr, k, 0, counter, true);
             k--;
         }
     } else if (sortOrder === "desc") {
+
         while (i >= 0) {
-            heapifyMin(arr, length, i, counter);
+            heapify(arr, length, i, counter, false);
             i--;
         }
 
         while (k >= 0) {
             counter.swaps++;
             [arr[0], arr[k]] = [arr[k], arr[0]];
-            heapifyMin(arr, k, 0, counter);
+            heapify(arr, k, 0, counter, false);
             k--;
         }
     } else {
         console.log("Wrong sort order")
     }
-
 
     const endTime = performance.now();
     counter.executionTime = endTime - startTime
